a0337485c31d798c91a6ffe1fafbfc297e86afaa
czdrxne <czdrxne@gmail.com>
2026-03-30 22:36:23 +0800

    Development update. Refactored display component for displaying preview result.

--- src/lib/components/Display.svelte
+++ src/lib/components/Display.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     export let value: string = "0";
+    export let preview: string = "";
 </script>
 
-<section class="bg-primary-bg w-full pb-3 pt-9 rounded-xl flex justify-end px-4 text-2xl sm:text-3xl text-white">
-    {value || "0"}
+<section class="text-right relative bg-primary-bg w-full pb-3 pt-12 rounded-xl px-4 text-2xl sm:text-3xl text-white">
+    <span class="absolute right-4 top-3 text-[1.4rem] text-neutral-700">{preview || ""}</span>
+    <span>{value || "0"}</span>
 </section>
