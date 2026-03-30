0d9db0ec8777ad618079fdec6f5ae307505d1393
czdrxne <czdrxne@gmail.com>
2026-03-30 12:16:05 +0800

    Development update. Created display component to display result.

---
+++ src/lib/components/Display.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let value: string = "0";
+</script>
+
+<section class="bg-primary-bg w-full pb-3 pt-9 rounded-xl flex justify-end px-4 text-2xl sm:text-3xl text-white">
+    {value || "0"}
+</section>
