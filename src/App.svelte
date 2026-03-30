a537974291324d31aa416b54d218135790bf11fe
czdrxne <czdrxne@gmail.com>
2026-03-30 12:59:57 +0800

    Development update. Added container component for app component for completion.

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Container from "@components/Container.svelte";
+</script>
+
+<main class="w-screen h-screen overflow-hidden p-2 flex items-center justify-center">
+    <Container />
+</main>
