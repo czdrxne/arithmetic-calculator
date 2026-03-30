d9e977b3bdee0f86d93c4f012dab281740ca2fe9
czdrxne <czdrxne@gmail.com>
2026-03-30 22:37:15 +0800

    Development update. Refactored container component for additional preview result logic.

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
     import Display from "@components/Display.svelte";
     import Buttons from "@components/Buttons.svelte";
-    import { init } from "@utilities/logic.ts";
+    import { initCalc, initPreview } from "@utilities/logic.ts";
 
     let value: string = "";
+    let preview: string = "";
 
-    const handlePress = (btn: string): string => value = init(value, btn);
+    function handlePress(btn: string): string {
+        value = initCalc(value, btn);
+        preview = initPreview(value);
+    };
 
 </script>
 
 <div class="h-fit sm:w-fit sm:p-4 p-3 bg-secondary-bg rounded-3xl space-y-4 text-white">
-    <Display value={value} />
+    <Display {value} {preview} />
     <Buttons on:press={(e) => handlePress(e.detail)} />
 </div>
