832aa1d292d4525166f9843956d62e655a6c1bfa
czdrxne <czdrxne@gmail.com>
2026-03-30 12:52:00 +0800

    Development update. Added child components and utility logic.

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+    import Display from "@components/Display.svelte";
+    import Buttons from "@components/Buttons.svelte";
+    import { init } from "@utilities/logic.ts";
+
+    let value: string = "";
+
+    const handlePress = (btn: string): string => value = init(value, btn);
+
 </script>
 
 <div class="h-fit sm:w-fit sm:p-4 p-3 bg-secondary-bg rounded-3xl space-y-4 text-white">
-    
+    <Display value={value} />
+    <Buttons on:press={(e) => handlePress(e.detail)} />
 </div>
