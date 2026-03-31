7a78247f851d1a1fa23f8e1ffc24fd920179143a
czdrxne <czdrxne@gmail.com>
2026-03-31 15:23:14 +0800

    Development update. Refactored container component since history component is added.

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,19 +1,49 @@
 <script lang="ts">
     import Display from "@components/Display.svelte";
     import Buttons from "@components/Buttons.svelte";
+    import History from "@components/History.svelte";
     import { initCalc, initPreview } from "@utilities/logic.ts";
-
+    
     let value: string = "";
     let preview: string = "";
+    let showHistory: boolean = false;
+
+    let history: { expr: string; result: string }[] = [];
 
     function handlePress(btn: string): string {
+        if (btn === "=") {
+            try {
+                const result = initCalc(value, btn);
+                history = [
+                    { expr: value, result },
+                    ...history
+                ];
+
+                value = result;
+                preview = "";
+                return;
+            } catch {
+                value = "Error";
+                return;
+            }
+        }
         value = initCalc(value, btn);
         preview = initPreview(value);
     };
 
+    const toggleHistory = () => showHistory = !showHistory;
+
 </script>
 
-<div class="h-fit sm:w-fit sm:p-4 p-3 bg-secondary-bg rounded-3xl space-y-4 text-white">
-    <Display {value} {preview} />
+<div class="relative h-fit sm:w-fit sm:p-4 p-3 bg-secondary-bg rounded-3xl space-y-4 text-white">
+    {#if showHistory}
+        <History {history} on:select={(e) => {
+            value = e.detail.expr;
+            preview = e.detail.result;
+            showHistory = false;
+        }} />
+    {/if}
+    <Display on:toggle={toggleHistory} {value} {preview} />
     <Buttons on:press={(e) => handlePress(e.detail)} />
 </div>
+
